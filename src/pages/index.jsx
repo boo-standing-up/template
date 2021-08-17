a6a7c1041bdb4655c22d66fefd0c5469576dae02
boo-standing-up <70996914+boo-standing-up@users.noreply.github.com>
2021-08-17 12:22:56 +0900

    画像下余白について

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,6 +15,30 @@ export default function Home() {
       </Head>
 
       <Header />
+      <main>
+        <div className="flex relative justify-center content-center items-center ">
+          <div className="container p-0 m-0 ">
+            <div className="flex flex-wrap items-center ">
+              <div className="p-0 m-0 w-full ">
+                <Image
+                  src="/vercel.svg"
+                  alt=""
+                  quality={100}
+                  width={1920}
+                  height={1080}
+                />
+                <Image
+                  src="/vercel.svg"
+                  alt=""
+                  quality={100}
+                  width={1920}
+                  height={1080}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
 
       <Footer />
       <footer className={styles.footer}>
